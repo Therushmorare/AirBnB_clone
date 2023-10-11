534b194e86b72a491f6b956815b89cfad57eab64
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-10-11 21:30:14 +0000

    base_model

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Amenity class."""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+	"""amenity.
+
+	Attributes:
+		name (str): Amenity name.
+	"""
+
+	name = ""
---
+++ models/base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""BaseModel class."""
+import models
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """BaseModel of the HBnB project."""
+
+	def __init__(self, *args, **kwargs):
+	"""BaseModel.
+
+	Args:
+		*args (any): value.
+		**kwargs (dict): Key/value pairs of attributes.
+	"""
+	time_format = "%Y-%m-%dT%H:%M:%S.%f"
+	self.id = str(uuid4())
+	self.created_at = datetime.today()
+	self.updated_at = datetime.today()
+	if len(kwargs) != 0:
+		for key, value in kwargs.items():
+			if key == "created_at" or key == "updated_at":
+				self.__dict__[key] = datetime.strptime(value, time_format)
+			else:
+				self.__dict__[k] = value
+	else:
+		models.storage.new(self)
+
+    def save(self):
+        """add date to filestorage."""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
+	def diction_store(self):
+		"""
+		Return BaseModel dictionary
+		"""
+		diction = self.__dict__.copy()
+		diction["created_at"] = self.created_at.isoformat()
+		diction["updated_at"] = self.updated_at.isoformat()
+		diction["__class__"] = self.__class__.__name__
+		return diction
+
+	def __str__(self):
+	"""Return string"""
+	string_repr = self.__class__.__name__
+	return "[{}] ({}) {}".format(string_repr, self.id, self.__dict__)
