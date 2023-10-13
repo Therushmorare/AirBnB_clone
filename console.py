6387b34a3ec52c01ef84277d5979f24d6d7f19df
Akuthi <akuthiapril99@gmail.com>
2023-10-13 15:40:56 +0000

    initial code for the console

---
+++ console.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module defines the HBNB console
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+	"""
+	The HBNBCommand class defines the HBNB command interpreter
+
+	Args:
+		prompt (str): command line prompt
+	"""
+
+	prompt = "(hbnb) "
+	
+	def do_quit(self, arg):
+		"""
+		Quit command to exit the program
+		"""
+		return True
+
+	def do_EOF(self, arg):
+		"""
+		EOF command to exit the program
+		"""
+		print("")
+		return True
+		
+
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
