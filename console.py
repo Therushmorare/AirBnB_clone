5e54d152e3dae39c2423cc4b78badd50688b7f1c
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-10-13 16:15:15 +0000

    create method

--- console.py
+++ console.py
@@ -28,9 +28,19 @@ class HBNBCommand(cmd.Cmd):
 		"""
 		print("")
 		return True
-		
 
-
+	def do_create(self, arg):
+		"""
+		Create function
+		"""
+		argl = parse(arg)
+		if len(argl) == 0:
+			print("** class name missing **")
+		elif argl[0] not in HBNBCommand.__classes:
+			print("** class doesn't exist **")
+		else:
+			print(eval(argl[0])().id)
+			storage.save()
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
