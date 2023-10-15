17701b6d1111a3f3494af34641e3ae977311848d
Akuthi <akuthiapril99@gmail.com>
2023-10-15 23:19:37 +0000

    on to the checker

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,31 +10,39 @@ from models.amenity import Amenity
 from models.review import Review
 
 class FileStorage:
-    	"""FileStorage class.
-    	Attributes:
-        	__file_path (str): The name of file path.
-        	__objects (dict): A dictionary.
-    	"""
-    	__file_path = "file.json"
-    	__objects = {}
+	"""FileStorage class.
+	Attributes:
+		__file_path (str): The name of file path.
+		__objects (dict): A dictionary.
+	"""
+	__file_path = "file.json"
+	__objects = {}
 
-    	def all(self):
+	@property
+	def file_path(self):
+		return self.__file_path
+
+	@property
+	def objects(self):
+		return self.__objects
+
+	def all(self):
         	"""Return  __objects."""
         	return FileStorage.__objects
 
-    	def new(self, obj):
+	def new(self, obj):
         	"""Set new"""
         	object_name = obj.__class__.__name__
         	FileStorage.__objects["{}.{}".format(object_name, obj.id)] = obj
 
-    	def save(self):
+	def save(self):
         	"""save method"""
         	storage = FileStorage.__objects
         	object_dictionary = {obj: storage[obj].to_dict() for obj in storage.keys()}
         	with open(FileStorage.__file_path, "w") as f:
             		json.dump(object_dictionary, f)
 
-    	def reload(self):
+	def reload(self):
         	"""reload method."""
         	try:
             		with open(FileStorage.__file_path) as f:
